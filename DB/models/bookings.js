cec065a249b8e899512f887b7f2542e0598d0e58
Ammar Amin <ammar.amin1975@gmail.com>
2022-05-09 23:22:37 +0200

    add bookingsSchema structure

---
+++ DB/models/bookings.js
@@ -0,0 +1,22 @@
+import { Schema, model, models } from "mongoose";
+
+const bookingsSchema = new Schema({
+  id: Number,
+  screeningid: Number,
+  movieid: Number,
+  salonid: Number,
+  date: String,
+  time: String,
+  seatsmap: Boolean[[]],
+  users: [
+    {
+      userid: Number,
+      seatrow: Number,
+      seatcolumn: Number,
+      payment: Boolean
+    },
+  ],
+});
+
+const Bookings = models.Bookings || model("Bookings", bookingsSchema);
+export default Bookings;
