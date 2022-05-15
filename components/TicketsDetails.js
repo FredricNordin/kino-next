80b74e5b0973400d2166e3f5a9127acac30a610c
Ammar Amin <ammar.amin1975@gmail.com>
2022-05-15 05:43:34 +0200

    couldn't handle seats indiviually yet

--- components/TicketsDetails.js
+++ components/TicketsDetails.js
@@ -1,30 +1,43 @@
 import SalonDetails from "./SalonDetails";
 import styles from "../styles/SalonDetails.module.css";
-import { useState } from "react/cjs/react.production.min";
+import { useState, useEffect } from "react";
 
 export default function TicketsDetails({ booking, salon }) {
 
   const [seatCondition, setSeatCondition] = useState(booking.seatsmap);
+  console.log("seatCondition = ", seatCondition);
 
   // Seat Value
   function handleOneSeat(seat) {
     seat = !seat;
     console.log("seat after click = ", seat);
-
   };
+
+  // useEffect(() => {
+  //   // Update the seatsmap 
+  //   booking.seatsmap = booking.seatsmap.forEach(element => {
+  //     element = !element;
+  //   });;
+  //   console.log("seatsmap inside useEffect = ", booking.seatsmap);
+  // });
 
   // Seat List
   function handleSeats(rrow) {
     var j = 0;
     const seatsList = rrow.map((seat) => {
-      console.log("seat before click = ", seat);
+      // console.log("seat before click = ", seat);
       j++;
       return (
         <ul className={styles["row-seats"]}>
           <li key={j} className={styles["single-seat"]}>
             <button
-              onClick={handleOneSeat(seat)}
-              className={styles["free -seat"]}>
+              onClick={() => {
+                seat = !seat;
+                setSeatCondition(!seatCondition);
+                console.log("setSeatCondition = ", seatCondition);
+                // console.log("seat (", i, ",", j, ") after click = ", seat);
+              }}
+              className={seat ? styles["booked-seat"] : styles["free-seat"]}>
               {seat ? "[x]" : "[_]"}
             </button>
           </li>
@@ -36,8 +49,8 @@ export default function TicketsDetails({ booking, salon }) {
 
   // Row List
   var i = 0;
-  const rowsList = salon.seatsmap.map((row) => {
-    console.log("row", i, "= ", row);
+  const rowsList = booking.seatsmap.map((row) => {
+    // console.log("row", i, "= ", row);
     i++;
     const rrow = row;
     return (
